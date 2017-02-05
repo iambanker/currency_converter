4be32bfb1eae99eba0ca5d096318f59521fba728
iambanker <skazeneovocie@gmail.com>
2017-02-05 10:05:01 +0100

    Moved flask manager under main function

--- manager.py
+++ manager.py
@@ -1,9 +1,14 @@
 from app import create_app
 
-from flask_script import Manager
-
 app = create_app()
-manager = Manager(app)
+
+
+def main():
+    from flask_script import Manager
+
+    manager = Manager(app)
+    manager.run()
+
 
 if __name__ == '__main__':
-    manager.run()
+    main()
